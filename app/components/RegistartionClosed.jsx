6ec964ea45fae6815ba751185f16d20f3ce78446
Vikas Phulriya <vikasphulariya@gmail.com>
2023-11-14 10:40:38 +0000

    Registration closed

---
+++ app/components/RegistartionClosed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RegistrationClosed = ({ accountHolder, accountNumber, ifscCode }) => {
+    return (
+      <div className="bank-account-details">
+        <h2>Registration Closed</h2>
+      </div>
+    );
+  };
+  
+  export default RegistrationClosed;
+  
--- app/page.js
+++ app/page.js
@@ -2,18 +2,20 @@ import Overview from "./components/Overview";
 import Form from "./components/form";
 import BankAccountDetails from "./components/BankAccountDetails";
 import QrScreen from "./components/QrScreen";
+import RegistrationClosed from "./components/RegistartionClosed";
 export default function Home(params) {
   return (
     <>
-    <div className="mx-2 flex flex-col container  rounded-md justify-center w-full">
-      <Overview />
-      <Form />
-      {/* <QrScreen data={"vikas"}/> */}
-      <BankAccountDetails
-        accountHolder="Novel Research Found"
-        accountNumber="00158-58000-08501"
-        ifscCode="YESB0000015"
-      />
+      <div className="mx-2 flex flex-col container  rounded-md justify-center w-full">
+        <RegistrationClosed />
+        {/* <Overview />
+        <Form /> */}
+        {/* <QrScreen data={"vikas"}/> */}
+        {/* <BankAccountDetails
+          accountHolder="Novel Research Found"
+          accountNumber="00158-58000-08501"
+          ifscCode="YESB0000015"
+        /> */}
       </div>
     </>
   );
